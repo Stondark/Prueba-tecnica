54bf3d75d02be4a43bf08f25625892b059de21bd
Stondark <pipegamo55@gmail.com>
2022-06-07 12:40:44 -0500

    BD reporte 1 añadida junto con su consulta

--- logica/metodos.php
+++ logica/metodos.php
@@ -5,11 +5,11 @@ Class Metodos{
     public function multiplosDeUnNumero( int $num ){
         return array_sum(array_filter((range(1,$num - 1)), function($tmp){
             return $tmp % 3 == 0 || $tmp % 5 == 0;}));
-
-
         // Invocamos el método array_sum el cuál le pasaremos como parámetro
         // otro método array_filter el cuál nos ayudará a filtrar los múltiplos de 3 y 5, le pasamos como parámetro
-        // la creación de un array con el método range y la función que nos retornará los múltiplos
+        // la creación de un array con el método range que nos retornará un array con la siguiente estructura
+        // Array ( [0] => 1 [1] => 2 [2] => 3 [3] => 4 [4] => 5 [5] => 6 [6] => 7 [7] => 8 [8] => 9 ... n-1)
+        //y el otro parámetro es la función que nos retornará los múltiplos del número
     }
 
     public function invertirPalabras(string $string): string{
